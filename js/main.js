162c85d370546d96f47d857f2c6887f6727a6840
Josh Hardy <josh@godeldesign.com>
2014-03-15 20:04:39 +1100

    finished

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+$( document ).ready(function() {
+ console.log('would go here');
+    
+var initialDelay = 800;
+var delayAmt = 500;
+    
 
+    $('.show-on-load').delay(initialDelay).queue(function(){
+        $(this).addClass('fade-in').clearQueue();
+    });
+
+ 
+//   $('.show-on-load').addClass('fade-in'); 
+//    $('#hopium-text').delay(initialDelay).slideUp(delayAmt).fadeIn(delayAmt);
+    $('.youtube').delay(initialDelay).fadeIn(delayAmt);
+    $('.soundcloud').delay(initialDelay+delayAmt).fadeIn(delayAmt);
+    $('.contact').delay(initialDelay+delayAmt+delayAmt).fadeIn(delayAmt);
+    
+    
+});
